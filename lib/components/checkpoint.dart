5b043d437f7652411a9d522efd600e1759803d9b
Yeonho Jang <siabard@gmail.com>
2024-01-25 08:40:48 +0900

    경고내역 제거

--- lib/components/checkpoint.dart
+++ lib/components/checkpoint.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:pixel_adventure/components/player.dart';
 import 'package:pixel_adventure/pixel_adventure.dart';
 
